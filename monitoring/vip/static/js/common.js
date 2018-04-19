fb0faf00e84a4de4b54014fa8080aeddc3af81e0
cnzhangfan <574854941@qq.com>
2018-04-19 15:40:31 +0800

    ssss

--- monitoring/vip/static/js/common.js
+++
@@ -1,7 +0,0 @@
-$('.nav li').hover(function(){
-	$(this).css('background','#000');
-	$(this).find('a').css('color','#FFD100');
-},function(){
-	$(this).css('background','#FFD100');
-	$(this).find('a').css('color','#000');
-})
