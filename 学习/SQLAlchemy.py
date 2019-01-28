23205787477f3eb665c534ae81f5a650d4a78e6b
unknown <vzhoufei@qq.com>
2019-01-28 11:49:44 +0800

    sss

--- 学习/SQLAlchemy.py
+++ 学习/SQLAlchemy.py
@@ -56,7 +56,7 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
 from sqlalchemy.dialects.mysql.types import TINYINT
 Base = declarative_base()
-engine = create_engine("mysql+mysqlconnector://root:@127.0.0.1:3306/test?charset=utf8", max_overflow=5)
+engine = create_engine("mysql+mysqlconnector://root:@127.0.0.1:3306/test?charset=utf8", max_overflow=5,echo=True)#echo=True输出sql
 class Users(Base):
 	__tablename__ = 'users'
 	id = Column(Integer, primary_key=True)
@@ -90,13 +90,16 @@ session.commit()
 #简单查询    
 print(session.query(User).all())
 print(session.query(User.name, User.fullname).all())    
-print(session.query(User, User.name).all())        
+print(session.query(User, User.name).all())   
+
+
 
 
 #带条件查询    
 print(session.query(User).filter_by(name='user1').all())    
 print(session.query(User).filter(User.name == "user").all())    
-print(session.query(User).filter(User.name.like("user%")).all())      
+print(session.query(User).filter(User.name.like("user%")).all())
+print(session.query(User).filter(User.name == "user")) #返回sql语句   
 
   
 #多条件查询    
@@ -151,3 +154,80 @@ session.query(func.count(User.id)).\
 from sqlalchemy import distinct
 # count distinct "name" values
 session.query(func.count(distinct(User.name)))
+
+
+#获取表结构
+from sqlalchemy.engine import reflection
+insp = reflection.Inspector.from_engine(engine)
+colums=insp.get_columns('user_groups')#这里是写的表名
+for i in colums:
+        print(i)
+
+
+
+#查询单条
+session.query(User).filter(User.name=="test").first()
+session.query(User).filter(User.name=="test").one()#有多条结果是会报错
+
+#limit 
+print( session.query(User).filter(User.username!='jingqi').limit(2).all())
+print( session.query(User).filter(User.username!='jingqi').offset(2).all())
+print( session.query(User).filter(User.username!='jingqi').slice(2,3).all())
+
+
+#orderby
+session.query(Users).filter(Users.userName=='test').order_by(Users.uid).order_by(Users.userName).slice(2,5).all()
+from sqlalchemy import desc
+session.query(Users).filter(Users.userName=='test').order_by(desc(Users.uid)).all()
+
+#不等于
+print( session.query(User).filter(User.username!='jingqi').all() )
+#模糊匹配 like
+print( session.query(User).filter(User.username.like('jingqi')).all() )
+print( session.query(User).filter(User.username.notlike('jingqi')).all() )
+
+#成员属于  in_
+print( session.query(User).filter(User.username.in_(['jingqi','jingqi1'])).all() )
+
+#成员不属于  notin_
+print( session.query(User).filter(User.username.notin_(['jingqi','jingqi2'])).all() )
+
+#空判断
+print( session.query(User).filter(User.username==None).all() )
+print( session.query(User).filter(User.username.is_(None)).all() )
+print( session.query(User).filter(User.username.isnot(None)).all() )
+
+#多条件
+print( session.query(User).filter(User.username.isnot(None),User.password=='qwe123').all() )
+
+#选择条件
+from sqlalchemy import or_,and_,all_,any_
+print( session.query(User).filter(or_(User.username=='jingqi',User.password=='qwe123')).all() )
+print( session.query(User).filter(and_(User.username=='jingqi2',User.password=='111')).all() )
+
+
+#多表联查
+print( session.query(UserDetails,User).all() )  #这个是 cross join
+print( session.query(UserDetails,User).filter(User.id==UserDetails.id).all() )  #这是也是cross join 但是加上了where条件
+data = session.query(Users,Column_).filter(Users.uid==4).filter(Users.userName==Column_.cname).all()
+print(data[0].Users.userName)
+#这个是inner join
+print( session.query(User.username,UserDetails.lost_login).join(UserDetails,UserDetails.id==User.id).all() )  
+#这个才是左连接，sqlalchemy没有右连接
+print( session.query(User.username,UserDetails.lost_login).outerjoin(UserDetails,UserDetails.id==User.id).all() )  
+
+#这个是union关联
+q1 = session.query(User.id)
+q2 = session.query(UserDetails.id)
+print(q1.union(q2).all())  
+
+
+#原生查询 获取数据后，数据会删除
+sql = "select * from users"
+data = session.execute(sql)
+print(data)
+for i in data:
+    print(i)
+print( data.fetchone() )
+print( data.fetchmany() )
+print( data.fetchall() )
